8985a1519a25496723c8de3f00c7be9e29f33cb6
Tietang Wang <wtt.2012@gmail.com>
2013-07-01 15:50:52 +0800

    SWFUpload - Fork from SWFUpload Build 2.5.0 beta3.2 Original Project at: http://code.google.com/p/swfupload/

---
+++ swfupload_fp10/deploy.bat
@@ -0,0 +1,8 @@
+@echo off
+copy swfupload.js    ..\samples\demos\swfupload
+copy swfupload.swf    ..\samples\demos\swfupload
+copy ..\plugins\*.js  ..\samples\demos\swfupload
+
+copy swfupload.js    ..\test\swfupload
+copy swfupload.swf    ..\test\swfupload
+copy ..\plugins\*.js  ..\test\swfupload
